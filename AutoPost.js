3dfafb00578e05839f55ddcc3d1c230074ddba55
nirvana <nirvandil@gmail.com>
2016-01-16 13:20:10 +0300

    Initial commit

---
+++ AutoPost.js
@@ -0,0 +1,26 @@
+
+var messageStrings = ['Здравствуйте. \nСейчас посмотрим.', 'Пожалуйста, в случае возникновения ' +
+'проблем - обращайтесь.'];
+var messageInputForm = document.getElementsByName('message')[0];
+var buttonAddMessage = document.querySelectorAll('input[value="Добавить"]')[0];
+var hello = document.createElement('input');
+hello.type = 'button';
+hello.value = 'Прив';
+hello.addEventListener('click', function() { addReply(0); } );
+var by = document.createElement('input');
+by.type = 'button';
+by.value = 'Пока';
+by.addEventListener('click', function() { addReply(1); } );
+var message = document.getElementsByName('message')[0];
+if (message) {
+    message.parentElement.appendChild(hello);
+    message.parentElement.appendChild(by);
+    message.addEventListener('keydown', function (event){
+        if (event.ctrlKey && event.keyCode == 13)
+            buttonAddMessage.click();
+    })
+}
+function addReply(string_num) {
+    messageInputForm.value = messageStrings[string_num];
+    buttonAddMessage.click();
+}
