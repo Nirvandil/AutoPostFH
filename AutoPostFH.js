60bc2d2491ec08a4331374618f9ab3c7f7f55c7b
nirvandil <nirvandil@gmail.com>
2016-01-22 01:40:07 +0300

    Small refactoring

--- AutoPostFH.js
+++ AutoPostFH.js
@@ -7,24 +7,30 @@ var messageInputForm = document.getElementsByName('message')[0];
 var buttonAddMessage = document.querySelectorAll('input[value="Добавить"]')[0];
 var buttonChangeMessage = document.querySelectorAll('input[value="Изменить"]')[0];
 var hello = document.createElement('input');
+var by = document.createElement('input');
+var passIncorrect = document.createElement('input');
 hello.type = 'button';
 hello.value = 'Прив';
-hello.addEventListener('click', function() { addReply(0); } );
-var by = document.createElement('input');
 by.type = 'button';
 by.value = 'Пока';
-by.addEventListener('click', function() { addReply(1); } );
-var passIncorrect = document.createElement('input');
 passIncorrect.type = 'button';
 passIncorrect.value = 'Pass';
-passIncorrect.addEventListener('click', function(){addReply(2)});
+hello.addEventListener('click', function () {
+    addReply(0);
+});
+by.addEventListener('click', function () {
+    addReply(1);
+});
+passIncorrect.addEventListener('click', function () {
+    addReply(2)
+});
 var message = document.getElementsByName('message')[0];
 if (message) {
     var placeForButtons = message.parentElement.parentElement;
     placeForButtons.appendChild(hello);
     placeForButtons.appendChild(by);
     placeForButtons.appendChild(passIncorrect);
-    message.addEventListener('keydown', function (event){
+    message.addEventListener('keydown', function (event) {
         if (event.ctrlKey && event.keyCode == 13) {
             if (messageInputForm.value == '')
                 alert("Нельзя отправить пустое сообщение!");
@@ -33,9 +39,9 @@ if (message) {
         }
     })
 }
-if (buttonChangeMessage){
-    message.addEventListener('keydown', function(event){
-        
+if (buttonChangeMessage) {
+    message.addEventListener('keydown', function (event) {
+
     })
 }
 function addReply(string_num) {
