6e83a671a71eb91b2f9343766804fde0b477c22d
nirvandil <nirvandil@gmail.com>
2016-01-30 01:59:56 +0300

    Small refactoring

--- AutoPostFH.js
+++ AutoPostFH.js
@@ -1,8 +1,7 @@
 var messageStrings = ['Здравствуйте. \nСейчас посмотрим.', 'Пожалуйста, в случае возникновения ' +
 'проблем или если будет нужна наша помощь - обращайтесь.', 'Пароль пользователя root, который' +
-' вы указали при создании тикета, не подходит. Укажите, пожалуйста, корректный (если вы его не ' +
-'изменяли, он может быть найден в письме с доступами, ' +
-'которое приходило вам после заказа сервера)'];
+' вы указали при создании тикета, не подходит. Предоставьте, пожалуйста, корректный (если вы его не изменяли, ' +
+'он может быть найден в письме с доступами, которое приходило вам после заказа сервера)'];
 var messageInputForm = document.getElementsByName('message')[0];
 var buttonAddMessage = document.querySelectorAll('input[value="Добавить"]')[0];
 var buttonChangeMessage = document.querySelectorAll('input[value="Изменить"]')[0];
@@ -31,7 +30,7 @@ var message = document.getElementsByName('message')[0];
 if (message) {
     var table  = document.getElementsByTagName('tbody')[4];
     var buttonsRow = document.createElement('tr');
-    table.insertBefore(buttonsRow, table.rows[table.rows.length]);
+    table.insertBefore(buttonsRow, table.rows[1]);
     var cellHello = document.createElement('td');
     var cellPass = document.createElement('td');
     var cellBy = document.createElement('td');
