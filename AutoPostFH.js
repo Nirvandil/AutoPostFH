6dc12dbd34118f49dee9a1b1b50a961eb5b58fcd
nirvandil <nirvandil@gmail.com>
2016-02-09 23:01:41 +0300

    Fix missed messageStrings[3]

--- AutoPostFH.js
+++ AutoPostFH.js
@@ -1,14 +1,16 @@
 var messageStrings = ['Здравствуйте. \nСейчас посмотрим.', 'Пожалуйста, в случае возникновения ' +
 'проблем или если будет нужна наша помощь - обращайтесь.', 'Пароль пользователя root, который' +
 ' вы указали при создании тикета, не подходит. Предоставьте, пожалуйста, корректный (если вы его не изменяли, ' +
-'он может быть найден в письме с доступами, которое приходило вам после заказа сервера)'];
+'он может быть найден в письме с доступами, которое приходило вам после заказа сервера)',
+'Здравствуйте. На данный момент наблюдаются проблемы с сетью на уровне датацента. Над их устра' +
+'нением уже работают. Приносим извинения за предоставленные неудобства'];
 var messageInputForm = document.getElementsByName('message')[0];
 var buttonAddMessage = document.querySelectorAll('input[value="Добавить"]')[0];
 var buttonChangeMessage = document.querySelectorAll('input[value="Изменить"]')[0];
 var hello = addButton('Прив', 0);
 var by = addButton('Пока', 1);
 var passIncorrect = addButton('Pass', 2);
-var test = addButton('test', 3);
+var dcnet = addButton('dcnet', 3);
 var message = document.getElementsByName('message')[0];
 if (message) {
     var table  = document.getElementsByTagName('tbody')[4];
@@ -17,7 +19,7 @@ if (message) {
     var cellHello = document.createElement('td');
     var cellPass = document.createElement('td');
     var cellBy = document.createElement('td');
-    var cellTest = document.createElement('td');
+    var cellDcNet = document.createElement('td');
     //Next 7 lines - special for scrolling down
     var scrollButton = document.createElement('input');
     scrollButton.type = 'button';
@@ -30,11 +32,11 @@ if (message) {
     buttonsRow.appendChild(cellHello);
     buttonsRow.appendChild(cellPass);
     buttonsRow.appendChild(cellBy);
-    buttonsRow.appendChild(cellTest);
+    buttonsRow.appendChild(cellDcNet);
     cellHello.appendChild(hello);
     cellPass.appendChild(passIncorrect);
     cellBy.appendChild(by);
-    cellTest.appendChild(test);
+    cellDcNet.appendChild(dcnet);
     message.addEventListener('keydown', function (event) {
         if (event.ctrlKey && event.keyCode == 13) {
             if (messageInputForm.value == '')
