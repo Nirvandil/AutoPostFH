ee63ea84c0e6b20764d32d232d9c49531885793f
nirvana <nirvandil@gmail.com>
2016-04-02 16:37:36 +0300

    Full redesign and refactoring

--- AutoPostFH.js
+++ AutoPostFH.js
@@ -1,41 +1,69 @@
-// ==UserScript==
-// @name        AutoPostAndColour
-// @namespace   FriendHosting
-// @include     https://friendhosting.net/admin_bar*
-// @version     1
-// @grant       none
-// ==/UserScript==
 document.body.style.background = 'lightgrey';
-var messageStrings = [
-    '\u0417\u0434\u0440\u0430\u0432\u0441\u0442\u0432\u0443\u0439\u0442\u0435. \n\u0421\u0435\u0439\u0447\u0430\u0441 \u043f\u043e\u0441\u043c\u043e\u0442\u0440\u0438\u043c.',
-    '\u041f\u043e\u0436\u0430\u043b\u0443\u0439\u0441\u0442\u0430, \u0432 \u0441\u043b\u0443\u0447\u0430\u0435 \u0432\u043e\u0437\u043d\u0438\u043a\u043d\u043e\u0432\u0435\u043d\u0438\u044f ' +
-    '\u043f\u0440\u043e\u0431\u043b\u0435\u043c \u0438\u043b\u0438 \u0435\u0441\u043b\u0438 \u0431\u0443\u0434\u0435\u0442 \u043d\u0443\u0436\u043d\u0430 \u043d\u0430\u0448\u0430 \u043f\u043e\u043c\u043e\u0449\u044c - \u043e\u0431\u0440\u0430\u0449\u0430\u0439\u0442\u0435\u0441\u044c.',
-    '\u041f\u0430\u0440\u043e\u043b\u044c \u043f\u043e\u043b\u044c\u0437\u043e\u0432\u0430\u0442\u0435\u043b\u044f root, \u043a\u043e\u0442\u043e\u0440\u044b\u0439' +
-    ' \u0432\u044b \u0443\u043a\u0430\u0437\u0430\u043b\u0438 \u043f\u0440\u0438 \u0441\u043e\u0437\u0434\u0430\u043d\u0438\u0438 \u0442\u0438\u043a\u0435\u0442\u0430, \u043d\u0435 \u043f\u043e\u0434\u0445\u043e\u0434\u0438\u0442. \u041f\u0440\u0435\u0434\u043e\u0441\u0442\u0430\u0432\u044c\u0442\u0435, \u043f\u043e\u0436\u0430\u043b\u0443\u0439\u0441\u0442\u0430, \u043a\u043e\u0440\u0440\u0435\u043a\u0442\u043d\u044b\u0439 (\u0435\u0441\u043b\u0438 \u0432\u044b \u0435\u0433\u043e \u043d\u0435 \u0438\u0437\u043c\u0435\u043d\u044f\u043b\u0438, ' +
-    '\u043e\u043d \u043c\u043e\u0436\u0435\u0442 \u0431\u044b\u0442\u044c \u043d\u0430\u0439\u0434\u0435\u043d \u0432 \u043f\u0438\u0441\u044c\u043c\u0435 \u0441 \u0434\u043e\u0441\u0442\u0443\u043f\u0430\u043c\u0438, \u043a\u043e\u0442\u043e\u0440\u043e\u0435 \u043f\u0440\u0438\u0445\u043e\u0434\u0438\u043b\u043e \u0432\u0430\u043c \u043f\u043e\u0441\u043b\u0435 \u0437\u0430\u043a\u0430\u0437\u0430 \u0441\u0435\u0440\u0432\u0435\u0440\u0430)',
-    '\u0417\u0434\u0440\u0430\u0432\u0441\u0442\u0432\u0443\u0439\u0442\u0435. \u041d\u0430 \u0434\u0430\u043d\u043d\u044b\u0439 \u043c\u043e\u043c\u0435\u043d\u0442 \u043d\u0430\u0431\u043b\u044e\u0434\u0430\u044e\u0442\u0441\u044f \u043f\u0440\u043e\u0431\u043b\u0435\u043c\u044b \u0441 \u0441\u0435\u0442\u044c\u044e \u043d\u0430 \u0443\u0440\u043e\u0432\u043d\u0435 \u0434\u0430\u0442\u0430\u0446\u0435\u043d\u0442\u0430. \u041d\u0430\u0434 \u0438\u0445 \u0443\u0441\u0442\u0440\u0430' +
-    '\u043d\u0435\u043d\u0438\u0435\u043c \u0443\u0436\u0435 \u0440\u0430\u0431\u043e\u0442\u0430\u044e\u0442. \u041f\u0440\u0438\u043d\u043e\u0441\u0438\u043c \u0438\u0437\u0432\u0438\u043d\u0435\u043d\u0438\u044f \u0437\u0430 \u043f\u0440\u0435\u0434\u043e\u0441\u0442\u0430\u0432\u043b\u0435\u043d\u043d\u044b\u0435 \u043d\u0435\u0443\u0434\u043e\u0431\u0441\u0442\u0432\u0430'
-];
-var messageInputForm = document.getElementsByName('message') [0];
-var buttonAddMessage = document.querySelectorAll('input[value="\u0414\u043e\u0431\u0430\u0432\u0438\u0442\u044c"]') [0];
-var buttonChangeMessage = document.querySelectorAll('input[value="\u0418\u0437\u043c\u0435\u043d\u0438\u0442\u044c"]') [0];
-var hello = addButton('\u041f\u0440\u0438\u0432', 0);
-var by = addButton('\u041f\u043e\u043a\u0430', 1);
-var passIncorrect = addButton('Pass', 2);
-var dcnet = addButton('dcnet', 3);
+var buttonAddMessage = document.querySelectorAll('input[value="Добавить"]') [0];
 var message = document.getElementsByName('message') [0];
 if (message) {
     var tables = document.getElementsByTagName('table');
-    //\u041a\u043d\u043e\u043f\u043a\u0430 \u0432 \u043f\u0430\u043d\u0435\u043b\u044c \u0443\u043f\u0440\u0430\u0432\u043b\u0435\u043d\u0438\u044f:
-    var orderText = document.getElementsByTagName('td') [7].textContent;
+    var orderText = ((document.getElementsByTagName('td') [7].textContent.search('Заказ')) > 0)?document.getElementsByTagName('td')[7].textContent:document.getElementsByTagName('td')[6].textContent;
     var address = orderText.match(/([0-9]{1,3}[\.]){3}[0-9]{1,3}/);
-
-    if (address) {
+    //aimRow - place (tr) where we'll place our new menu and buttons
+    var aimRow = document.getElementsByTagName('tbody') [4].children[4];
+    addToPanelButtons(address);
+    addScrollDownButton();
+    addMenuToPage(aimRow);
+    addPostButtonToPage(aimRow);
+    message.addEventListener('keydown', function (event) {
+        if (event.ctrlKey && event.keyCode == 13) {
+            if (message.value == '')
+                alert('Нельзя добавить пустое сообщение!');
+            else if (!buttonChangeMessage)
+                buttonAddMessage.click();
+        }
+    })
+}
+//Create Menu and Options
+function createMenu()
+{
+    var dropDownMenu = document.createElement('select');
+    dropDownMenu.setAttribute('id', 'dropDownMenu');
+    var helloOption = document.createElement('option');
+    var passOption = document.createElement('option');
+    var byOption = document.createElement('option');
+    var dcTroublesOption = document.createElement('option');
+    helloOption.value = 'Здравствуйте. Сейчас посмотрим.';
+    passOption.value = 'Пароль пользователя root, который вы указали при создании тикета, не подходит, укажите, пожалуйста, корректный. \n' +
+        'Если вы его не изменяли, он может быть найден в письме, которое приходило вам после заказа сервера.';
+    byOption.value = 'Пожалуйста, в случае возникновения проблем или если будет нужна наша помощь - обращайтесь.' ;
+    dcTroublesOption.value = 'Здравствуйте. На данный момент имеются проблемы с сетью на уровне датацентра. Нади их устранением уже работают.\n' +
+        'Приносим извинения за доставленные неудобства.' ;
+    helloOption.text = 'Привет';
+    passOption.text = 'Пароль';
+    byOption.text = 'Пока';
+    dcTroublesOption.text = 'Сеть';
+    dropDownMenu.appendChild(helloOption);
+    dropDownMenu.appendChild(passOption);
+    dropDownMenu.appendChild(byOption);
+    dropDownMenu.appendChild(dcTroublesOption);
+    return dropDownMenu;
+}
+function postAnswer ()
+{
+    var menu = document.getElementById('dropDownMenu');
+    message.value = menu.options[menu.selectedIndex].value;
+    buttonAddMessage.click();
+}
+//Add Menu to page and button with listener to post
+function addMenuToPage(aimRow)
+{
+    aimRow.appendChild(createMenu());
+}
+function addToPanelButtons(hasAddress)
+{
+    if (hasAddress){
         var toVesta = document.createElement('input');
         toVesta.type = 'button';
         toVesta.value = 'To VESTA';
         toVesta.addEventListener('click', function () {
-                window.open("https://"+address[0]+":8083");
+            window.open("https://"+address[0]+":8083");
         });
         var toIsp = document.createElement('input');
         toIsp.type = 'button' ;
@@ -46,15 +74,10 @@ if (message) {
         tables[0].appendChild(toVesta);
         tables[0].appendChild(toIsp);
     }
-    //\u041a\u043e\u043d\u0435\u0446 \u043a\u043d\u043e\u043f\u043a\u0438 \u0432 \u043f\u0430\u043d\u0435\u043b\u044c \u0443\u043f\u0440\u0430\u0432\u043b\u0435\u043d\u0438\u044f
-    var table = document.getElementsByTagName('tbody') [4];
-    var buttonsRow = document.createElement('tr');
-    table.insertBefore(buttonsRow, table.rows[1]);
-    var cellHello = document.createElement('td');
-    var cellPass = document.createElement('td');
-    var cellBy = document.createElement('td');
-    var cellDcNet = document.createElement('td');
-    if (document.body.scrollHeight > 1800) {
+}
+function addScrollDownButton()
+{
+    if (document.body.scrollHeight > 1080) {
         //Next 7 lines - special for scrolling down
         var scrollButton = document.createElement('input');
         scrollButton.type = 'button';
@@ -64,37 +87,14 @@ if (message) {
         });
         tables[0].appendChild(scrollButton);
     }
-    buttonsRow.appendChild(cellHello);
-    buttonsRow.appendChild(cellPass);
-    buttonsRow.appendChild(cellBy);
-    buttonsRow.appendChild(cellDcNet);
-    cellHello.appendChild(hello);
-    cellPass.appendChild(passIncorrect);
-    cellBy.appendChild(by);
-    cellDcNet.appendChild(dcnet);
-    message.addEventListener('keydown', function (event) {
-        if (event.ctrlKey && event.keyCode == 13) {
-            if (messageInputForm.value == '')
-                alert('\u041d\u0435\u043b\u044c\u0437\u044f \u043e\u0442\u043f\u0440\u0430\u0432\u0438\u0442\u044c \u043f\u0443\u0441\u0442\u043e\u0435 \u0441\u043e\u043e\u0431\u0449\u0435\u043d\u0438\u0435!');
-            else
-                buttonAddMessage.click();
-        }
-    })
 }
-if (buttonChangeMessage) {
-    message.addEventListener('keydown', function (event) {
-    })
-}
-function addReply(string_num) {
-    messageInputForm.value = messageStrings[string_num];
-    buttonAddMessage.click();
-}
-function addButton(name, answerNumber) {
-    var addedButton = document.createElement('input');
-    addedButton.type = 'button';
-    addedButton.value = name;
-    addedButton.addEventListener('click', function () {
-        addReply(answerNumber)
+function addPostButtonToPage(aimRow)
+{
+    var addPost = document.createElement('input');
+    addPost.type = 'button';
+    addPost.value = 'Запостить';
+    addPost.addEventListener('click', function(){
+        postAnswer();
     });
-    return addedButton;
+    aimRow.appendChild(addPost);
 }
